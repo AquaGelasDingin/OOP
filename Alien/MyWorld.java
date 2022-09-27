ed923c943de0b55d9537fcd8140c0257561ef601
Subhan Maulana <113961979+AquaGelasDingin@users.noreply.github.com>
2022-09-27 12:27:15 +0800

    Project OOP Alien
    Tes Project di pertemuan ke 3

---
+++ Alien/MyWorld.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Hero hero = new Hero();
+        addObject(hero,48,158);
+        hero.setLocation(39,194);
+        
+        Hero hero2 = new Hero();
+        addObject(hero2,50,130);
+        hero2.setLocation(39,271);
+        
+        Hero hero3 = new Hero();
+        addObject(hero3,70,130);
+        hero3.setLocation(39,121);
+    }
+}
